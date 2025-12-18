e33aab32904eb7af519fd89ee9509bb38fdeb3dd
prathikshaa80 <prathikshaap19@gmail.com>
2025-12-18 14:10:47 +0530

    AttendanceTrackerApp.jsx

---
+++ AttendanceTrackerApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AttendanceTracker from "./AttendanceTracker";
+
+const App = () => {
+  return (
+    <div>
+      <AttendanceTracker />
+    </div>
+  );
+};
+
+export default App;
