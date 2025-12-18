6b7f11ad4bff18191184cb55f4e8b5f34d972a99
prathikshaa80 <prathikshaap19@gmail.com>
2025-12-18 14:11:37 +0530

    TaskDasboardApp.jsx

---
+++ TaskDasboardApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskDashboard from "./TaskDashboard";
+
+const App = () => {
+  return (
+    <div>
+      <TaskDashboard />
+    </div>
+  );
+};
+
+export default App;
