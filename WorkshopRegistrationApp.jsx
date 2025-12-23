485366abec73c5b34bb8dbf9ee0855bd2db6f2fb
prathikshaa80 <prathikshaap19@gmail.com>
2025-12-23 16:20:37 +0530

    Create WorkshopRegistrationApp.jsx

---
+++ WorkshopRegistrationApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WorkshopRegistration from "./WorkshopRegistration";
+
+function App() {
+  return (
+    <div>
+      <WorkshopRegistration />
+    </div>
+  );
+}
+
+export default App;
