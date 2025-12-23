9db37458c0cedf96faea313b581de7e4d9a117ff
prathikshaa80 <prathikshaap19@gmail.com>
2025-12-23 15:23:24 +0530

    EventRegistrationApp.jsx

---
+++ EventRegistrationApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventRegistration from "./EventRegistration";
+
+function App() {
+  return (
+    <div>
+      <EventRegistration />
+    </div>
+  );
+}
+
+export default App;
